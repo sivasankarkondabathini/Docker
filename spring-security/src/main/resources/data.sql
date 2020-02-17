305d6b65aa1473ae8411a1c232606943c6851493
Vinod Egallapati <VEgallapati@techwave.net>
2020-02-17 19:17:02 +0530

    add spring boot apps

---
+++ spring-security/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users VALUES ('jill', '$2a$04$qUlqAEEYF1YvrpJMosodoewgL6aO.qgHytl2k5L7kdXEWnJsFdxvq', TRUE);
+INSERT INTO users VALUES ('username', '$2a$04$qUlqAEEYF1YvrpJMosodoewgL6aO.qgHytl2k5L7kdXEWnJsFdxvq', TRUE);
+INSERT INTO authorities VALUES ('jill', 'USERS');
+INSERT INTO authorities VALUES ('username', 'USERS');
+
